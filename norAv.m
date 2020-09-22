004c2504c5717f43ced03af9977a453835b82c67
MSR806 <msr0806@users.noreply.github.com>
2020-09-22 10:20:50 +0530

    Create norAv.m

---
+++ norAv.m
@@ -0,0 +1,27 @@
+clear all;
+close all;
+
+a = imread('cameraman.tif');
+a = double(a);
+
+[m,n] = size(a);
+
+msk = input("Type the size of the normal average mask filter");
+
+mm = (msk-1)/2;
+
+b = zeros(m+2*mm, n+2*mm);
+
+for i = 1+mm:m+mm
+    for j = 1+mm:n+mm
+        b(i,j)=a(i-mm,j-mm);
+    end
+end
+%imshow(b,[])
+for i = 1+mm:m
+    for j = 1+mm:n
+        c=b(i-mm:i+mm,j-mm:j+mm);
+        b(i,j) = mean(mean(c));
+    end
+end
+imshow(b,[])
